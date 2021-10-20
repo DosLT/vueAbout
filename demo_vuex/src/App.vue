e547b23c2eefc6103054ab37e8d831d2a245f84e
sijiangli <2933622123@qq.com>
2021-10-21 00:20:51 +0800

    update demo_vuex add router-link

--- demo_vuex/src/App.vue
+++ demo_vuex/src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div id="app">
+    <router-link
+      :style="{ marginRight: '10px' }"
+      v-for="item in routes"
+      :key="item.path"
+      :to="item.path"
+      >{{ item.name }}</router-link
+    >
     <router-view />
   </div>
 </template>
+
+<script>
+import { routes } from "./router";
+export default {
+  name: "App",
+  data() {
+    return {
+      routes,
+    };
+  },
+};
+</script>
